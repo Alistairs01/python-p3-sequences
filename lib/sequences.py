59298894ea6ddb6fa622af062d8d2133cae9f4b9
Alistairs <alistairs.gatheru@student.moringaschool.com>
2024-05-25 10:38:30 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # Check if the requested length is less than or equal to 0
+    if length <= 0:
+        print([])
+        return
+    
+    #Initailize a list of the specified length with all the elements set to o
+    fibonacci_sequence = [0] * length
+    # if the length is greater than 1 , set the second element to 1
+    if length > 1 :
+        fibonacci_sequence[1] = 1
+
+        #Calculate each fibonacci number from the third element onwards 
+        for i in range(2, length):
+            #Each element is the sum of the two preceding elements 
+            fibonacci_sequence[i] = fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2]
+#return the list containing the sequence 
+    print(fibonacci_sequence) 
+
+print(print_fibonacci(9))
